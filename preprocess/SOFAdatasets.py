2c193931f487a2e105e810e1fcfed6f6116790d7
Yusen Li <166271193+tongjiRain@users.noreply.github.com>
2026-07-21 17:38:17 +0800

    Update SOFAdatasets.py

--- preprocess/SOFAdatasets.py
+++ preprocess/SOFAdatasets.py
@@ -5,6 +5,7 @@ from natsort import natsorted
 import librosa
 
 DATASET_PATH = "../data/"
+TARGET_SAMPLING_RATE = 44100
 
 class SOFADataset(Dataset):
     def __init__(self):
@@ -40,10 +41,10 @@ class SOFADataset(Dataset):
         orig_sr = HRTF.Data.SamplingRate.get_values()[0]  # 初始采样率
         locations = HRTF.Source.Position.get_values(system="spherical") # (方位角,俯仰角, 距离半径）
         orig_hrirs = HRTF.Data.IR.get_values()  # 原始HRIR
-        if orig_sr == 44100:
+        if orig_sr == TARGET_SAMPLING_RATE:
             return locations, orig_hrirs
         else:
-            resample_hrirs = librosa.resample(orig_hrirs, orig_sr=orig_sr, target_sr=44100)
+            resample_hrirs = librosa.resample(orig_hrirs, orig_sr=orig_sr, target_sr=TARGET_SAMPLING_RATE)
             return locations, resample_hrirs
 
 
@@ -182,6 +183,28 @@ class Crossmod(SOFADataset):
         return os.path.join(self.sofa_dir, "IRC_%s_C_44100.sofa" % subject_ID)
 
 
+class SONICOM(SOFADataset):
+    def __init__(self):
+        super(SONICOM, self).__init__()
+        self.name = "sonicom"
+        self.sofa_dir = os.path.join(DATASET_PATH, "SONICOM/sofa")
+        self._expand_basic_info()
+
+    def _get_all_sofa_files_from_dir(self):
+        files_96k = glob.glob(os.path.join(self.sofa_dir, "P*_FreeFieldComp_96kHz.sofa"))
+        files_48k = glob.glob(os.path.join(self.sofa_dir, "P*_FreeFieldCompMinPhase_48kHz.sofa"))
+        return files_96k if files_96k else files_48k
+
+    def _get_ID_from_sofa_path(self, path):
+        return os.path.basename(path).split("_")[0][1:]
+
+    def _get_sofa_path_from_ID(self, subject_ID):
+        candidate_96k = os.path.join(self.sofa_dir, "P%s_FreeFieldComp_96kHz.sofa" % subject_ID)
+        if os.path.exists(candidate_96k):
+            return candidate_96k
+        return os.path.join(self.sofa_dir, "P%s_FreeFieldCompMinPhase_48kHz.sofa" % subject_ID)
+
+
 
 
 
@@ -190,7 +213,6 @@ if __name__ == "__main__":
     location ,HRIR  = master_dataset.__getitem__(0)
     print(location.shape)
     print(HRIR.shape)
-
 
 
 
